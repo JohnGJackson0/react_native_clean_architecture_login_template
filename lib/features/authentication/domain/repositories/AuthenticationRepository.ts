5385d5598f36638923cfd7f5cc9e0a71994d9305
johngjackson0 <johngjackson0@gmail.com>
2023-01-30 08:14:00 -0500

    feat: setup domain models for signup

---
+++ lib/features/authentication/domain/repositories/AuthenticationRepository.ts
@@ -0,0 +1,10 @@
+import UserSignUpModel from '../entities/UserSignUpModel';
+
+/*
+  There is no enforcement for names of functions and parameter
+  types in typescript
+*/
+
+export default interface AuthenticationRepository {
+  userSignUp(email: string, password: string): Promise<UserSignUpModel>;
+}
---
+++ lib/features/authentication/domain/usecases/SignUpUseCase.ts
@@ -0,0 +1,13 @@
+import AuthenticationRepository from '../repositories/AuthenticationRepository';
+
+export default class SignUpUseCase {
+  _repository: AuthenticationRepository;
+
+  constructor(repo: AuthenticationRepository) {
+    this._repository = repo;
+  }
+
+  public execute = async (email: string, password: string) => {
+    await this._repository.userSignUp(email, password);
+  };
+}
